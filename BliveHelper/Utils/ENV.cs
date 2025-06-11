b7730b8bc7315ee84d3969f65554fe6b359ce78b
Kashimura <kashimura@qq.com>
2025-06-11 23:09:34 +0800

    Update: 新增记录接收到弹幕消息

--- BliveHelper/Utils/ENV.cs
+++ BliveHelper/Utils/ENV.cs
@@ -1,7 +1,10 @@
 ﻿using BilibiliDM_PluginFramework;
 using BliveHelper.Utils.Blive;
 using BliveHelper.Utils.Obs;
+using BliveHelper.Utils.Structs;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -13,6 +16,7 @@ namespace BliveHelper.Utils
         public static Config Config { get; } = new Config();
         public static BliveAPI BliveAPI { get; } = new BliveAPI();
         public static BliveInfo BliveInfo { get; } = new BliveInfo();
+        public static ObservableCollection<Danmaku> Danmakus { get; } = new ObservableCollection<Danmaku>();
 
         public static string AppDllFileName { get; } = Assembly.GetExecutingAssembly().Location;
         public static string AppDllFilePath { get; } = new FileInfo(AppDllFileName).DirectoryName;
@@ -28,6 +32,28 @@ namespace BliveHelper.Utils
             BliveAPI.Cookies = Config.Cookies;
             // 尝试启动 WebSocket 服务
             WebSocket.Connect(Config.WebSocket.ServerUrl, Config.WebSocket.ServerKey);
+        }
+
+        public static async void AddDanmaku(DanmakuModel danmaku)
+        {
+            var danmakuData = danmaku.RawDataJToken.ToObject<DanmakuRawData>().Data;
+            // 如果之前记录已经获取到 UID, 则直接覆盖
+            var hasUIDUser = Danmakus.FirstOrDefault(x => x.UserId != 0 && x.UserName == danmaku.UserName);
+            if (hasUIDUser != null)
+            {
+                danmakuData.UserId = hasUIDUser.UserId;
+            }
+            else
+            {
+                var uids = await BliveAPI.NameToUID(new string[] { danmaku.UserName });
+                danmakuData.UserId = uids.FirstOrDefault()?.UID ?? 0;
+            }
+            // 记录当前弹幕
+            Danmakus.Insert(0, danmakuData);
+            if (Danmakus.Count > 100)
+            {
+                Danmakus.RemoveAt(Danmakus.Count - 1);
+            }
         }
 
         public static void Log(string message) => DanMuPlugin?.Log(message);
