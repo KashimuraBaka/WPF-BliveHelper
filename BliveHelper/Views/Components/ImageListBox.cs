e524080c0bfd64134979bf2840f8cef0bbbff441
Kashimura <kashimura@qq.com>
2025-06-17 14:22:10 +0800

    Update; 新增图片列表, 用于固定排列显示图片

---
+++ BliveHelper/Views/Components/ImageListBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BliveHelper.Views.Components
+{
+    internal class ImageListBox : ListBox
+    {
+        public int ItemColumn
+        {
+            get => (int)GetValue(ItemColumnProperty);
+            set => SetValue(ItemColumnProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemColumnProperty = DependencyProperty.Register(
+            nameof(ItemColumn),
+            typeof(int),
+            typeof(ImageListBox),
+            new PropertyMetadata(1)
+        );
+
+        public Thickness ItemMargin
+        {
+            get => (Thickness)GetValue(ItemMarginProperty);
+            set => SetValue(ItemMarginProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemMarginProperty = DependencyProperty.Register(
+            nameof(ItemMargin),
+            typeof(Thickness),
+            typeof(ImageListBox),
+            new PropertyMetadata(new Thickness(0))
+        );
+    }
+}
