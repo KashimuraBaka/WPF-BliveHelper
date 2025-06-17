64ca6873e8acf424fc9e73a93183a4a95da4df2a
Kashimura <kashimura@qq.com>
2025-06-17 14:23:11 +0800

    Update: 新增通用数值转为 Boolean 转换器

---
+++ BliveHelper/Utils/Converter/ValueToBooleanConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BliveHelper.Utils.Converter
+{
+    internal class ValueToBooleanConverter : IValueConverter
+    {
+        public bool Inverse { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = false;
+
+            if (value is bool boolValue) result = boolValue;
+            else if (value is int intValue) result = intValue > 0;
+            else if (value is float floatValue) result = floatValue > 0;
+            else if (value is double doubleValue) result = doubleValue > 0;
+            else if (value is string stringValue) result = !string.IsNullOrWhiteSpace(stringValue);
+            else if (value != null) result = true;
+
+            return !Inverse ? result : !result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
