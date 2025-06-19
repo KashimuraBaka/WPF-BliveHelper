8dd392267367b4dae9396b53f7f53feb4e762103
Kashimura <kashimura@qq.com>
2025-06-19 18:28:43 +0800

    Update: 用于存储 SVG 数据

---
+++ BliveHelper/Views/Components/PathAssist.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace BliveHelper.Views.Components
+{
+    internal class PathAssist
+    {
+
+        public static readonly DependencyProperty DataProperty = DependencyProperty.RegisterAttached(
+            "Data",
+            typeof(Geometry),
+            typeof(PathAssist),
+            new PropertyMetadata(default(Geometry))
+        );
+
+        public static Geometry GetData(DependencyObject element)
+        {
+            return (Geometry)element.GetValue(DataProperty);
+        }
+
+        public static void SetData(DependencyObject element, Geometry value)
+        {
+            element.SetValue(DataProperty, value);
+        }
+    }
+}
