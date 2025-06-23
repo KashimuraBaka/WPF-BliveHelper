481ed820f1533a85a3c1f13487b7e0535a778149
Kashimura <kashimura@qq.com>
2025-06-23 16:26:22 +0800

    Update: 整合DLL信息

--- BliveHelper/Utils/ENV.cs
+++ BliveHelper/Utils/ENV.cs
@@ -8,18 +8,22 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BliveHelper.Utils
 {
     public static class ENV
     {
-        public static DMPlugin DanMuPlugin { get; set; }
+        public static Main Plugin { get; set; }
         public static Config Config { get; } = new Config();
         public static BliveAPI BliveAPI { get; } = new BliveAPI();
         public static BliveInfo BliveInfo { get; } = new BliveInfo();
         public static ObservableCollection<Danmaku> Danmakus { get; } = new ObservableCollection<Danmaku>();
 
-        public static string AppDllFileName { get; } = Assembly.GetExecutingAssembly().Location;
+        public static Window AppWindow => Application.Current?.MainWindow;
+        public static Assembly AppAssembly { get; } = Assembly.GetExecutingAssembly();
+        public static string AppVersion => AppAssembly.GetName().Version.ToString();
+        public static string AppDllFileName => AppAssembly.Location;
         public static string AppDllFilePath { get; } = new FileInfo(AppDllFileName).DirectoryName;
         public static string ConfigDirectory { get; } = Path.Combine(AppDllFilePath, "BliveHelper");
         public static string ConfigFileName { get; } = Path.Combine(ConfigDirectory, "config.json");
@@ -70,6 +74,6 @@ namespace BliveHelper.Utils
             }
         }
 
-        public static void Log(string message) => DanMuPlugin?.Log(message);
+        public static void Log(string message) => Plugin?.Log(message);
     }
 }
